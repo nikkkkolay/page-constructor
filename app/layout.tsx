7e4ec6274dbce78ba389ccfa9ad4e95229cd033a
kkkk <ggn.f763@gmail.com>
2025-05-22 22:44:59 +0300

    настрокай проекта сборка страницы

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
+import "./fix.scss";
+import "@gravity-ui/page-constructor/styles/styles.scss";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = Montserrat({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -24,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+      <body className={`${geistSans.variable}`}>{children}</body>
     </html>
   );
 }
